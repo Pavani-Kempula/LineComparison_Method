449e15f2776ea881d8021490b98a7caf250d8816
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-08 03:59:20 +0530

    ADDED : Welcome To Line Comparison Program

---
+++ src/main/java/com/blz/linecomparison/LineComparison.java
@@ -0,0 +1,12 @@
+package com.blz.linecomparison;
+
+public class LineComparison {
+
+    public static void main(String[] args) {
+
+        //Displaying Welcome Message for User
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+
+}
